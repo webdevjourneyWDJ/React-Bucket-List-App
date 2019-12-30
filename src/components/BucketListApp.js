809a79c1fa88a5047aae64d347beef9b5c02452e
Gamaliel Barron <gbarronmm@gmail.com>
2019-12-30 10:13:11 -0800

    splitting up app.js into seperate files

---
+++ src/components/BucketListApp.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import AddOption from './Addoption'
+import Header from './Header'
+import Action from './Action'
+import Options from './Options'
+
+export default class BucketListApp extends React.Component {
+    constructor(props){
+        super(props);
+        this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
+        this.handlePick = this.handlePick.bind(this);
+        this.handleAddOption = this.handleAddOption.bind(this);
+        this.handleDeleteOption = this.handleDeleteOption.bind(this);
+        this.state = {
+            options: props.options
+        }
+    }
+
+    componentDidMount() { 
+        try {
+            const json = localStorage.getItem('options');
+            const options = JSON.parse(json);
+
+            if(options){
+                this.setState(() => ({options}));
+            }
+        } catch (error) {}      
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if(prevState.options.length !== this.state.options.length){
+            const json = JSON.stringify(this.state.options); 
+            localStorage.setItem("options", json);
+        }
+    }
+
+    componentWillUnmount() {
+        console.log("componentWillUnmount");        
+    }
+
+    handleDeleteOptions() {
+        this.setState(() => ({options: []}))
+    }
+
+    handleDeleteOption(optionToRemove){
+        this.setState((prevState) => {
+            return {
+                options: prevState.options.filter((option) => optionToRemove !== option)
+            }
+        })      
+    }
+
+    handlePick() {
+        const randNum = Math.floor(Math.random() * this.state.options.length);
+        const option = this.state.options[randNum]; 
+        alert(option)
+    }
+
+    handleAddOption(option) {
+        if(!option){
+            return 'Enter an actual string';
+        }else if(this.state.options.indexOf(option) > -1){
+            return 'this option is in the array'
+        }
+
+        this.setState((prevState) => ({options:prevState.options.concat(option)}))
+    }
+    render() {
+        const subtitle = "Make a computer decide your way of life!";
+
+        return (
+            <div>
+                <Header subtitle={subtitle}/>
+                <Action 
+                    hasOptions={this.state.options.length > 0}
+                    handlePick = {this.handlePick}   
+                />
+                <Options 
+                    options={this.state.options}
+                    handleDeleteOptions = {this.handleDeleteOptions}
+                    handleDeleteOption={this.handleDeleteOption} 
+                />
+                <AddOption handleAddOption={this.handleAddOption}/>
+            </div>
+        )
+    }
+}
+
+BucketListApp.defaultProps = {
+    options: []
+}
+
+
---
+++ src/components/Options.js
@@ -0,0 +1,20 @@
+import React from 'react'
+// import Option from './Option'
+
+const Options = (props) => {
+    return (
+        <div>
+        <button onClick={props.handleDeleteOptions}>Remove All</button>
+            {
+               props.options.map((option, index) => (
+                   <Option 
+                    key={index} 
+                    optionText={option} 
+                    handleDeleteOption={props.handleDeleteOption}
+                   />)) 
+            }
+        </div>
+    )
+}
+
+export default Options;
